9e9f63a3cecc678ed340c871b0320ac63f669b03
Aditya Joshi <adi3230@gmail.com>
2018-01-06 19:22:44 +0100

    chore(): add ng-bootstrap and provide router links to the dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
@@ -37,6 +38,7 @@ import { LoginComponent } from './login/login.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'products', component: ProductsComponent },
@@ -46,7 +48,7 @@ import { LoginComponent } from './login/login.component';
       { path: 'my-orders', component: MyOrdersComponent },
       { path: 'login', component: LoginComponent },
       { path: 'admin/products', component: AdminProductsComponent },
-      { path: 'admin/order', component:  AdminOrdersComponent}
+      { path: 'admin/orders', component:  AdminOrdersComponent}
     ])
   ],
   providers: [],
