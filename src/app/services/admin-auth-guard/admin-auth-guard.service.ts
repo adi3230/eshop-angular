26b0d93103ce82e592b5bf0d30da5d1db5d3898e
Aditya Joshi <adi3230@gmail.com>
2018-01-14 17:28:22 +0100

    feat(define admin guard): logic encapsulated in service

---
+++ src/app/services/admin-auth-guard/admin-auth-guard.service.ts
@@ -0,0 +1,20 @@
+import { UserService } from './../user/user.service';
+import { CanActivate } from '@angular/router/src/interfaces';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class AdminAuthGuardService implements CanActivate {
+
+  constructor(private authService: AuthService,
+              private userService: UserService) { }
+
+  canActivate(): Observable<boolean> {
+    return this.authService.user$
+      .switchMap(user => this.userService.get(user.uid))
+      .map(appUser => appUser.isAdmin);
+  }
+}
