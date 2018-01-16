48e8d9e09369cf6546295e9c0fd513b6329be19b
Aditya Joshi <adi3230@gmail.com>
2018-01-17 00:10:21 +0100

    feat(categories): service to retrieve categories from firebase

---
+++ src/app/services/category/category.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Injectable()
+export class CategoryService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  getCategories() {
+    return this.db.list('/categories', {
+      query: {
+        orderByChild: 'name'
+      }
+    });
+  }
+
+}
