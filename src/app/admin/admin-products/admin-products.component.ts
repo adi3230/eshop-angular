026955bcf21e006d9ccca3f1bdce5c0dedcfa0ff
Aditya Joshi <adi3230@gmail.com>
2018-01-18 14:30:00 +0100

    feat(filter): logic to retrieve filter products and display, create interface for products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,16 +1,32 @@
 import { ProductService } from './../../services/product/product.service';
 import { Component, OnInit } from '@angular/core';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+      .subscribe(
+        subscribedProducts => this.filteredProducts = this.products = subscribedProducts);
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit() {
---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  title: string;
+  price: string;
+  category: string;
+  imageUrl: string;
+}
