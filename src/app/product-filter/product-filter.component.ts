0caea7e38cd34355d98ce4473605cea1db6a6d64
Aditya Joshi <adi3230@gmail.com>
2018-01-20 22:14:38 +0100

    refact(products):move category into a component

---
+++ src/app/product-filter/product-filter.component.ts
@@ -0,0 +1,18 @@
+import { CategoryService } from './../services/category/category.service';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'product-filter',
+  templateUrl: './product-filter.component.html',
+  styleUrls: ['./product-filter.component.css']
+})
+export class ProductFilterComponent implements OnInit {
+  categories$;
+  @Input('category') category;
+
+  constructor(categoryService: CategoryService) {
+    this.categories$ = categoryService.getCategories();
+  }
+  ngOnInit() {
+  }
+}
