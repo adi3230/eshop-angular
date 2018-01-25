ef1a525804905fbf82ee933829dcd97b7382e2b5
Aditya Joshi <adi3230@gmail.com>
2018-01-25 22:56:08 +0100

    feat(cart): service to create cart on firebase

---
+++ src/app/services/shopping-cart/shopping-cart.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Injectable()
+export class ShoppingCartService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  create() {
+    return this.db.list('/shopping-carts').push({
+      dateCreated: new Date().getTime()
+    });
+  }
+
+}
