c1e2bfe1c58df5d075ff2bdb61ea4153b328ba48
Aditya Joshi <adi3230@gmail.com>
2018-03-17 23:14:01 +0100

    feat(shoppingCart): interfaces for shopping cart

---
+++ src/app/interfaces/shopping-cart-item.ts
@@ -0,0 +1,6 @@
+import { Product } from "./product";
+
+export interface ShoppIngCartItem {
+  product: Product;
+  quantity: number;
+}
---
+++ src/app/interfaces/shopping-cart.ts
@@ -0,0 +1,5 @@
+import { ShoppIngCartItem } from "./shopping-cart-item";
+
+export interface ShoppingCart {
+  items: ShoppIngCartItem[];
+}
