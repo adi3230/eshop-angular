3d1619014b5f60d1af7aec657615955f0de9c947
Aditya Joshi <adi3230@gmail.com>
2018-03-25 00:39:14 +0100

    fix(quanity): fix 0 quantity issue

--- src/app/services/shopping-cart/shopping-cart.service.ts
+++ src/app/services/shopping-cart/shopping-cart.service.ts
@@ -61,12 +61,18 @@ export class ShoppingCartService {
 
 
     item$.take(1).subscribe(item => {
+      let quantity = (item.quantity || 0) + change;
+
+      if (quantity === 0) {
+        item$.remove();
+      } else {
         item$.update({
           title: product.title,
           imageUrl: product.imageUrl,
           price: product.price,
-          quantity: (item.quantity || 0) + change
+          quantity: quantity
         });
+      }
     });
   }
 
