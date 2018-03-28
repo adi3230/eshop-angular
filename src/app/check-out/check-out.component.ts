8b0b73a29e09af86672ee7c3e563bf3897c42cf1
Aditya Joshi <adi3230@gmail.com>
2018-03-28 23:02:01 +0200

    feat(checkout): refactor to extract logic from order model

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -5,6 +5,7 @@ import { ShoppingCart } from './../interfaces/shopping-cart';
 import { ShoppingCartService } from './../services/shopping-cart/shopping-cart.service';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
+import { Order } from '../interfaces/order';
 
 @Component({
   selector: 'app-check-out',
@@ -37,23 +38,7 @@ export class CheckOutComponent implements OnInit, OnDestroy {
   }
 
   placeOrder() {
-    const order = {
-      userId: this.userId,
-      datePlaced: new Date().getTime(),
-      shipping: this.shipping || {},
-      items: this.cart.items.map(i => {
-        return {
-          product: {
-            title: i.title,
-            imageUrl: i.imageUrl,
-            price: i.price
-          },
-          quantity: i.quantity,
-          totalPrice: i.totalPrice
-        };
-      })
-
-    };
+    let order = new Order(this.userId, this.shipping, this.cart);
 
     this.orderService.storeOrder(order);
 
