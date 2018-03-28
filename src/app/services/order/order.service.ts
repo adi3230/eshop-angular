179730a550403d420541d6f03864853702c2332e
Aditya Joshi <adi3230@gmail.com>
2018-03-28 23:33:46 +0200

    feat(checkout): clearing cart after order success

--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -1,13 +1,16 @@
+import { ShoppingCartService } from './../shopping-cart/shopping-cart.service';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private shoppingCartService: ShoppingCartService, private db: AngularFireDatabase) { }
 
-  storeOrder(order) {
-    return this.db.list('/orders').push(order);
+  async placeOrder(order) {
+    let orderPlaced = await this.db.list('/orders').push(order);
+    this.shoppingCartService.clearCart();
+    return orderPlaced;
   }
 
 }
